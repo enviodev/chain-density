80c3896e56cdb20cc282780578cc9e384514b867
moose-code <jonjonclark@gmail.com>
2023-12-07 20:21:22 +0200

    Adding event and transaction types

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ NETWORK_URLS = {
     "okbc_testnet": "https://okbc-testnet.hypersync.xyz"
 }
 
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -40,12 +39,13 @@ def index():
     if request.method == 'POST':
         address = request.form['address']
         address = address.lower()
-        print(address)
+        request_type = request.form['type']
         selected_network = request.form['network']
         network_url = NETWORK_URLS.get(selected_network, "https://eth.hypersync.xyz")
         try:
-            directory = asyncio.run(fetch_data(address, selected_network, network_url))
-            img = create_plot(directory)
+            directory = asyncio.run(fetch_data(address, selected_network, network_url, request_type))
+            img = create_plot(directory, request_type)
+            # img = 'data:image/png;base64,./assets/sad-pepe.png'
             return render_template('plot.html', plot_url=img)
         except Exception as e:
             print(f"Error: {e}")
@@ -54,27 +54,49 @@ def index():
     return render_template('index.html')
 
 
-async def fetch_data(address, selected_network, network_url):
+async def fetch_data(address, selected_network, network_url, request_type):
     # Create hypersync client using the chosen hypersync endpoint
     client = hypersync.hypersync_client(network_url)
+    is_event_request = request_type == "event"
 
     # The query to run
-    query = {
-        "from_block": 0,
-        "logs": [{"address": [address]}],
-        "field_selection": {
-            "log": ["block_number", "log_index", "transaction_index"],
-        },
-    }
+    if is_event_request:
+        query = {
+            "from_block": 0,
+            "logs": [{"address": [address]}],
+            "field_selection": {
+                "log": ["block_number", "log_index", "transaction_index"],
+            },
+        }
+    else:
+        query = {
+            "from_block": 0,
+            "transactions": [
+                # We want all the transactions that come from this address
+                {"from": [address]},
+                # We want all the transactions that went to this address
+                {"to": [address]},
+            ],
+            "field_selection": {
+                "transaction": [
+                    "block_number",
+                    "transaction_index",
+                    "hash",
+                ],
+            },
+        }
 
     # Create a directory named after the address
-    directory = f"data_{selected_network}_{address}"
+    directory = f"data/data_{selected_network}_{request_type}_{address}"
     if not os.path.exists(directory):
         os.makedirs(directory)
         await client.create_parquet_folder(query, directory)
         print("Finished writing parquet folder")
     else:
-        if is_parquet_empty(f'{directory}/log.parquet'):
+        if is_event_request and is_parquet_empty(f'{directory}/log.parquet'):
+            await client.create_parquet_folder(query, directory)
+            print("Parquet previously wasn't successfully populated")
+        elif not is_event_request and is_parquet_empty(f'{directory}/transaction.parquet'):
             await client.create_parquet_folder(query, directory)
             print("Parquet previously wasn't successfully populated")
         else:
@@ -102,17 +124,22 @@ def round_based_on_magnitude(number):
         return round(number / 100000) * 100000
     
 
-def create_plot(directory):
-    plt.figure(figsize=(15, 9))  # Width, Height in inches
+def create_plot(directory, request_type):
+    plt.figure(figsize=(15, 7))  # Width, Height in inches
     # Read the log.parquet file
-    df = pd.read_parquet(f'{directory}/log.parquet')
+    is_event_request = request_type == "event"
+
+    if is_event_request:
+        df = pd.read_parquet(f'{directory}/log.parquet')
+    else:
+        df = pd.read_parquet(f'{directory}/transaction.parquet')
 
 
     # Define the interval size
     min_block = df['block_number'].min()
     max_block = df['block_number'].max()
 
-    interval_size = max(10000, round_based_on_magnitude((max_block - min_block)/ 50))
+    interval_size = max(5000, round_based_on_magnitude((max_block - min_block)/ 50))
 
     min_block_rounded = min_block - (min_block % interval_size)
 
@@ -150,6 +177,7 @@ def create_plot(directory):
     plot_url = base64.b64encode(buf.read()).decode('utf-8')
     buf.close()
     return f'data:image/png;base64,{plot_url}'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
--- read.py
+++ read.py
@@ -3,4 +3,5 @@ import pandas as pd
 df = pd.read_parquet('data_eth_0x5bf9dfb1b27c28e5a1d8e5c5385a1a353ec9d118/log.parquet')
 
 print(len(df))
-print(df.head(50))
+print(df.head(10))
+print(df.describe())
